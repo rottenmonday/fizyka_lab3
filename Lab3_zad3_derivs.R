dd5757db65d75c4a70965c6d7cdac56152902f9c
Aleksander Wiśniewski <arnachimm@gmail.com>
2020-05-29 13:40:25 +0200

    Fixed equations and niepewność masy wody

--- Lab3_zad3_derivs.R
+++ Lab3_zad3_derivs.R
@@ -11,13 +11,13 @@ cwkd <- function(m1h2o, cwh2o, mk, cwk, t1h2o, tk, ml){
 }
 
 mld <- function(m1h2o, cwh2o, mk, cwk, t1h2o, tk, ml){
-   return((m1h2o*cwh2o+mk*cwk)*(t1h2o-tk)/(ml^2))
+   return(-(m1h2o*cwh2o+mk*cwk)*(t1h2o-tk)/(ml^2))
 }
 
 tkd <- function(m1h2o, cwh2o, mk, cwk, t1h2o, tk, ml){
-   return((-tk)*(m1h2o*cwh2o+mk*cwk)/ml - cwh2o)
+   return((-1)*(m1h2o*cwh2o+mk*cwk)/ml - cwh2o)
 }
 
 t1h2od <- function(m1h2o, cwh2o, mk, cwk, t1h2o, tk, ml){
-   return(t1h2o*(m1h2o*cwh2o+mk*cwk)/ml)
+   return((m1h2o*cwh2o+mk*cwk)/ml)
 }
